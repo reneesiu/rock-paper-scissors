09d09544e94171c4457a826daca97f21f8c86881
Renee <ren.siu@gmail.com>
2021-10-27 16:50:28 -0500

    initial commit with one round

---
+++ rock-paper-scissors.js
@@ -0,0 +1,38 @@
+//computer randomly returns 'rock', 'paper' or 'scissors'
+function computerPlay() {
+    var playableOptions = ["rock", "paper", "scissors"];
+    randomSelection = Math.floor(Math.random() * playableOptions.length);
+    computerSelection = playableOptions[randomSelection];
+    return computerSelection;
+}
+//prompt for player selection
+function getPlayerInput() {
+    var playerSelection = prompt("Please make a selection");
+    return playerSelection;
+}
+
+
+//match computer selection to player selection and declare winner
+function playRound(computerSelection, playerSelection) {
+    console.log(computerSelection, playerSelection);
+    if (computerSelection === playerSelection) {
+        console.log("It's a tie!");
+    } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") || 
+        (computerSelection == "scissors" && playerSelection == "paper") || 
+        (computerSelection == "paper" && playerSelection == "rock")
+     ) {
+         console.log("You lose!");
+    } else if (
+        (computerSelection == "rock" && playerSelection == "paper") || 
+        (computerSelection == "scissors" && playerSelection == "rock") || 
+        (computerSelection == "paper" && playerSelection == "scissors")
+    ) {
+        console.log("You win!");
+    }
+} 
+
+
+playerSelection = getPlayerInput();
+computerSelection = computerPlay();
+playRound(computerSelection, playerSelection);
